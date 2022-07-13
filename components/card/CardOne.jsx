f811210218630a0a19fdc97cc5bbdaf6c82c4158
Renato Gomes dos Santos <39703265+renatogsantos@users.noreply.github.com>
2022-07-12 21:31:18 -0300

    construção de novos componentes

---
+++ components/card/CardOne.jsx
@@ -0,0 +1,15 @@
+import { Book } from "phosphor-react";
+
+export default function CardOne() {
+  return (
+    <div className="card-one">
+      <Book size={64} />
+      <p className="pt-4">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci modi,
+        ea quidem cupiditate iste porro nemo perferendis eveniet quasi deleniti
+        tempore totam blanditiis temporibus corrupti quibusdam ullam, hic
+        distinctio? Cum?
+      </p>
+    </div>
+  );
+}
