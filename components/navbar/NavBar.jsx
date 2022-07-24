e31a09ba76a2b67124223e798d5589026009d6fa
Renato Gomes dos Santos <39703265+renatogsantos@users.noreply.github.com>
2022-07-24 18:37:18 -0300

    ajustes

--- components/navbar/NavBar.jsx
+++ components/navbar/NavBar.jsx
@@ -1,18 +1,31 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo, List } from "phosphor-react";
+import {
+  FacebookLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  List,
+} from "phosphor-react";
 import { Container } from "react-bootstrap";
 
 export default function NavBar() {
   return (
     <div className="fixed-top mx-auto navbar-top">
-      <Container className="text-light py-3 d-flex align-items-center justify-content-between">
+      <Container className="text-light py-2 d-flex align-items-center justify-content-between">
         <div>
-          <List size={32} />
-          <small className="mx-2">Otec Contabilidade</small>
+          <a href="#" className="menu-button">
+            <List size={32} />
+          </a>
         </div>
+          <small className="mx-2">Otec Contabilidade</small>
         <div className="d-flex">
-          <LinkedinLogo size={24} />
-          <InstagramLogo size={24} />
-          <FacebookLogo size={24} />
+          <a href="" className="">
+            <LinkedinLogo size={24} />
+          </a>
+          <a href="" className="">
+            <InstagramLogo size={24} />
+          </a>
+          <a href="" className="">
+            <FacebookLogo size={24} />
+          </a>
         </div>
       </Container>
     </div>
